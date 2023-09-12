e6e46a304b5e0990047ef2275933de7b320c110a
BenFraga-work <144341646+BenFraga-work@users.noreply.github.com>
2023-09-12 10:54:09 +1000

    Update Card.js (2/3)

--- src/containers/Board/Cards/Card.js
+++ src/containers/Board/Cards/Card.js
@@ -8,10 +8,24 @@ const propTypes = {
 const galPng = require('../../../assets/images/gal.png');
 const delPng = require('../../../assets/images/del.png');
 
+function timeformat(timee) {
+  let ftime = "";
+  let past = false;
+  if (timee < 0) {timee = 0-timee; past = true}
+  if (timee/86400000 > 1) {ftime += Math.floor(timee/86400000) + 'd';};
+  let ttime = timee%86400000;
+  if (ttime/3600000 > 1) {ftime += Math.floor(ttime/3600000) + 'h';};
+  ttime = ttime%3600000;
+  if (ttime/60000 > 1) {ftime += Math.floor(ttime/60000) + 'm';};
+  ttime = ttime%60000;
+  ftime += Math.floor(ttime/1000) + 's '+ ["until","past"][Number(past)] + ' deadline';
+  return ftime;
+};
+function test(tests) {return tests}
 
 const Card = (props) => {
   const { style, item } = props;
-
+  let styledate = {background: ['#ff8888','#ffff88','#88ff88'][Number(item.timedue > Date.now()) + Number(item.timedue - 86400000 > Date.now())]}
   return (
     <div style={style} className="item" id={style ? item.id : null}>
       <div className="item-name">{item.title}</div>
@@ -23,6 +37,9 @@ const Card = (props) => {
           <div className="item-author">{`${item.firstName} ${item.lastName}`}</div>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, banditos.</p>
         </div>
+      </div>
+      <div className="item-deadline" style={styledate}>
+        <p>{timeformat(item.timedue-Date.now())}</p> {/*String(new Date(item.timedue)).split(' ').slice(0,5).join(' ') /n this is the worst way to do comments*/}
       </div>
       <div className="item-perfomers">
         <div className="add-perfomers">
